ed7bb2b72c7535e55fe2208680dd46986f423eeb
Steve Homer <226140217+HPPeterSteve@users.noreply.github.com>
2026-05-04 13:43:36 -0300

    vulnerabilidades corrigidas: pivot root, toctou e check

--- core_linux/diamondVaults.c
+++ core_linux/diamondVaults.c
@@ -2,7 +2,7 @@
  * diamondVaults.c
  *
  * VAULT SECURITY SYSTEM - Full Linux Implementation
- * Author: Peter Steve (architecture) | Senior Linux Engineer (implementation)
+ * Author: Peter Steve (architecture) | inux Engineer (implementation)
  * Date: 2026-04-11
  * Sandbox v2: 2026-04-19 — DiamondVault Hardened Sandbox Architecture
  *
